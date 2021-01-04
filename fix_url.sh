6ffb9c372b45aeb5c5514b0d40262826bda9c405
yoheia <yohei.az@gmail.com>
2021-01-05 01:13:17 +0900

    delete all files

--- fix_url.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-grep -l 'aboutme\/aboutme' ./public/*ml |xargs sed -i -e 's/aboutme\/aboutme/aboutme/g'
-grep -l 'aboutme\/aboutme' ./public/*/*ml |xargs sed -i -e 's/aboutme\/aboutme/aboutme/g'
-grep -l 'aboutme\/aboutme' ./public/*/*/*ml |xargs sed -i -e 's/aboutme\/aboutme/aboutme/g'
-
